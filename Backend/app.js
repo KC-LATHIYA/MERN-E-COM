4c4c83f5700122b99fb364d1410d7782fd35a0d5
Lathiya Krish <lathiyakrrish@gmail.com>
2025-12-11 16:07:10 +0530

    error fix

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,15 @@ import { wishlistRoutes } from "./routes/wishlistRoutes.js";
 import { errorHandler } from "./middleware/apiError.js";
 const app = express();
 
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["set-cookie"]
+  credentials: true
+}));
+// app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+// app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
